c8f501e41fe091b52be8483fe064d1c3320cf6e0
ivanov6444 <a.ivanov6444@gmail.com>
2022-12-01 18:17:19 +0200

    changing the structure of the application. implementation Lazy-loading feature modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-
-import { PizzasAllComponent } from './components/pizzas-all/pizzas-all.component';
-import {NotFoundPageComponent} from "./components/not-found-page/not-found-page.component";
 import {ShoppingCartComponent} from "./components/shopping-cart/shopping-cart.component";
 import {AdministrationComponent} from "./components/administration/administration.component";
 import {PizzaDetailsComponent} from "./components/pizza-details/pizza-details.component"
-
 import {AdminGuardService} from "./services/admin-guard.service";
 import {AdminGuard} from "./shared/administration.guard";
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/pizza', pathMatch: 'full' },
-  { path: 'pizza', component: PizzasAllComponent },
+  { path: '', redirectTo: 'pizza', pathMatch: 'full'},
+
+  { path: 'pizza',
+    pathMatch: 'full',
+    loadChildren: () => import('./components/pizzas-all/pizzas-all.module')
+      .then(module => module.PizzasAllModule)
+  },
+
   { path: 'pizza/:id', component: PizzaDetailsComponent},
   { path: 'shopping', component: ShoppingCartComponent },
   { path: 'admin', component: AdministrationComponent, canActivate: [AdminGuard] },
-  { path: '**', component: NotFoundPageComponent },
+
+  { path: '**',
+    loadChildren: () => import('./components/not-found-page/not-found-page.module')
+      .then(module => module.NotFoundPageModule)
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), CommonModule],
+  imports: [RouterModule.forChild(routes), CommonModule],
   exports: [RouterModule],
   providers: [AdminGuard, AdminGuardService]
 })
