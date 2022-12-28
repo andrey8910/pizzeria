b46be8735dfa84074fc1b34904f8119be04dda02
ivanov6444 <a.ivanov6444@gmail.com>
2022-12-28 13:23:45 +0200

    changed subscription method for IntersectionObserver in user page component

--- src/app/components/administration/admin-user-page/admin-user-page.component.ts
+++ src/app/components/administration/admin-user-page/admin-user-page.component.ts
@@ -1,25 +1,21 @@
 import {
-  AfterContentChecked, AfterContentInit,
-  AfterViewChecked,
   AfterViewInit,
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
   ElementRef,
-  OnInit, Renderer2
+  OnInit, QueryList, ViewChildren
 } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Location} from "@angular/common";
-import {Subscription} from "rxjs";
 import {UsersService} from "../../../shared/services/users.service";
-import {tap} from "rxjs/operators";
+import { tap} from "rxjs/operators";
 import {Users} from "../../../shared/interfaces/users";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {passEqual, ValidateLogin, ValidatePass, ValidatePassConfirm} from "../../../shared/Validators";
 import {OrdersService} from "../../../shared/services/orders.service";
 import {Orders} from "../../../shared/interfaces/orders";
 import {ProductsService} from "../../../shared/services/products.service";
-
 
 @Component({
   selector: 'app-admin-user-page',
@@ -28,35 +24,31 @@ import {ProductsService} from "../../../shared/services/products.service";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class AdminUserPageComponent implements OnInit, AfterViewInit, AfterViewChecked , AfterContentChecked, AfterContentInit{
-  public isEdit = false
-  public hidePass = true
-  public hideConfirmPass = true
-  public readyOrdersList = false
-  public userId: number
-  public userData: Users
-  public userOrders: Orders[] = []
-  public userOrdersUnitedProducts: any[] = []
-  public loader = false
-  private nextPage = 2
-  private subscription: Subscription;
+export class AdminUserPageComponent implements OnInit, AfterViewInit{
 
+  @ViewChildren('tableOrder') tableOrder: QueryList<any>;
+  public isEdit = false;
+  public hidePass = true;
+  public hideConfirmPass = true;
+  public readyOrdersList = false;
+  public userId: number;
+  public userData: Users;
+  public userOrders: Orders[] = [];
+  public userOrdersUnitedProducts: any[] = [];
+  public loader = false;
+  private nextPage = 2;
   public formEditUserData: FormGroup;
-
   public infiniteObserver = new IntersectionObserver(
     ([entry], observer) =>{
       if(entry.isIntersecting){
         observer.unobserve(entry.target)
         this.getUserOrders(this.nextPage++)
-        this.cdr.markForCheck()
       }
     },
     {
-      threshold: 0.5
+      threshold: 0.9
     }
   )
-
-
   constructor(
               private activateRoute: ActivatedRoute,
               private location: Location,
@@ -65,47 +57,31 @@ export class AdminUserPageComponent implements OnInit, AfterViewInit, AfterViewC
               private productsService: ProductsService,
               private elementRef: ElementRef,
               private cdr: ChangeDetectorRef,
-              private renderer: Renderer2
+
     ) { }
 
   ngOnInit(): void {
     this.initialization()
   }
-  ngAfterContentChecked(){
 
-  }
   ngAfterViewInit() {
-
+   this.tableOrder.changes.pipe(
+     tap(() => this.infiniteObserver.observe(this.tableOrder.last.nativeElement))
+   ).subscribe()
   }
 
-  ngAfterContentInit(){
-
-  }
-  ngAfterViewChecked(){
-
-  }
 
   private initialization(){
-    this.subscription = this.activateRoute.params.subscribe(params => this.userId = params['id']);
-    if(this.userId){
-      this.getUserData()
-      this.createFormEditUserData()
-      this.getUserOrders()
-    }
-
-      // const lastOrder: any = this.elementRef.nativeElement.querySelector('p-table tr.table-order:last-child')
-      // const observer = new MutationObserver((mutations:any) => {
-      //   mutations.forEach((mutation:any) => console.log(mutation));
-      // });
-      // observer.observe(lastOrder, {
-      //   attributes: true,
-      // });
-      //
-      // if(lastOrder){
-      //
-      //   this.infiniteObserver.observe(lastOrder)
-      //   this.cdr.markForCheck();
-      // }
+    this.activateRoute.params.pipe(
+      tap(params => this.userId = params['id']),
+      tap(() => {
+        if(this.userId){
+          this.getUserData()
+          this.createFormEditUserData()
+          this.getUserOrders()
+        }
+      })
+    ).subscribe();
 
   }
 
@@ -126,38 +102,25 @@ export class AdminUserPageComponent implements OnInit, AfterViewInit, AfterViewC
       password: new FormControl('', [Validators.required, ValidatePass]),
       confirmPassword: new FormControl('', [Validators.required, ValidatePassConfirm])
     }, passEqual('password', 'confirmPassword'))
-    this.cdr.markForCheck();
+
   }
-
-
 
   private getUserOrders(page = 1){
     this.ordersService.getOrdersByClientIdPageByPage(3, page, this.userId).pipe(
       tap((res:Orders[]) => {
-        this.readyOrdersList = false
+        this.readyOrdersList = false;
         if(res.length > 0){
-          this.userOrders.push(...res)
+          this.userOrders.push(...res);
+          this.getProductDataForOrderList(res);
           this.cdr.markForCheck();
-          this.getProductDataForOrderList()
-
-          console.log(res, 'res')
-          setTimeout(() => {
-            const lastOrder: any = this.elementRef.nativeElement.querySelectorAll('p-table tr.table-order:last-child').item(0)
-            if(lastOrder){
-              this.infiniteObserver.observe(lastOrder)
-              //this.cdr.markForCheck();
-            }
-          }, 1000);
         }
       })
     ).subscribe()
-    console.log(this.userOrders, 'userOrders')
-
   }
 
-  private getProductDataForOrderList(){
-    if(this.userOrders.length > 0){
-      this.userOrders.forEach((order : Orders) => {
+  private getProductDataForOrderList(ordersPage: Orders[]){
+    if(ordersPage.length > 0){
+      ordersPage.forEach((order : Orders) => {
         let orderChange: any = {
           orderNumber : order.id,
           createTime: order.creationTime,
@@ -174,7 +137,6 @@ export class AdminUserPageComponent implements OnInit, AfterViewInit, AfterViewC
             price: 0,
             params: {}
           }
-
           this.productsService.getPizzaById(listItem.productId).pipe(
             tap((res:any[]) => {
               productListItem.title = res[0].title
@@ -182,20 +144,19 @@ export class AdminUserPageComponent implements OnInit, AfterViewInit, AfterViewC
               productListItem.params = res[0].params
               orderChange.orderPrice += productListItem.price
               this.cdr.markForCheck();
-
             })
           ).subscribe()
           orderChange.productList.push(productListItem)
+          this.cdr.markForCheck();
         })
         this.userOrdersUnitedProducts.push(orderChange)
+        this.cdr.markForCheck();
       })
-
     }
-
     this.readyOrdersList = true
-
-
   }
+
+
 
 
 
