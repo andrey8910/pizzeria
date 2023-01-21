ee751ee0b0de72f4fcdc44fd0b1ca2709113e0d0
ivanov6444 <a.ivanov6444@gmail.com>
2023-01-21 12:57:48 +0200

    working with the new product data creation form

--- src/app/administration/admin-products/admin-products.component.ts
+++ src/app/administration/admin-products/admin-products.component.ts
@@ -6,6 +6,8 @@ import {ConfirmationService, ConfirmEventType} from 'primeng/api';
 import { MessageService } from 'primeng/api';
 import {Location} from "@angular/common";
 import {Subject} from "rxjs";
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {ValidateUrl} from "../../core/Validators";
 
 
 @Component({
@@ -20,6 +22,7 @@ export class AdminProductsComponent implements OnInit {
   public loader: boolean = false;
   public showCreateProduct = false;
   public products : Pizza[];
+  public formNewProduct: FormGroup;
 
 
 
@@ -27,15 +30,43 @@ export class AdminProductsComponent implements OnInit {
               private messageService: MessageService,
               private confirmationService : ConfirmationService,
               private cdr: ChangeDetectorRef,
+              private fb: FormBuilder,
               private location: Location,) { }
 
   ngOnInit(): void {
-    this.getPizzas()
+    this.getPizzas();
+    this.createFormNewProduct();
   }
 
   ngOnDestroy(){
     this.destroy$.next(true);
     this.destroy$.unsubscribe();
+  }
+
+  private createFormNewProduct(){
+    this.formNewProduct = this.fb.group({
+      title: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(30)]),
+      description: new FormControl('', [ Validators.required, Validators.minLength(6), Validators.maxLength(600)]),
+      ingredients: this.fb.array([]),
+      minPrice: new FormControl('', [Validators.required, Validators.min(100), Validators.max(2000)]),
+      minWeight: new FormControl('', [Validators.required, Validators.min(10), Validators.max(1500)]),
+      params: this.fb.group({
+        price: this.fb.array([]),
+        weight: this.fb.array([]),
+        size: this.fb.array([]),
+        title: this.fb.array([]),
+      }),
+      imageBig: new FormControl('', [Validators.required, ValidateUrl]),
+      imageMain: new FormControl('', [Validators.required, ValidateUrl])
+    })
+  }
+
+  get ingredients(): FormArray {
+    return this.formNewProduct.controls["ingredients"] as FormArray;
+  }
+
+  get params(): FormGroup{
+    return this.formNewProduct.controls["params"] as FormGroup;
   }
 
   private getPizzas(){
@@ -56,6 +87,10 @@ export class AdminProductsComponent implements OnInit {
   public addProduct(){
     this.showCreateProduct = true
     this.cdr.markForCheck()
+  }
+
+  public saveAddProduct(){
+    console.log(this.formNewProduct.value)
   }
 
   public canselAddProduct(){
@@ -84,6 +119,52 @@ export class AdminProductsComponent implements OnInit {
       }
     });
 
+  }
+
+  public addIngredient(){
+    const ingredientForm = this.fb.group<any>({
+      name: ['']
+    });
+    this.ingredients.push(ingredientForm);
+    this.cdr.markForCheck()
+  }
+
+  public saveIngredient(index: number, ingredientValue: string){
+    if(ingredientValue.length > 0){
+      this.ingredients.at(index).value.name = ingredientValue;
+    }
+    this.cdr.markForCheck()
+    console.log(index, ingredientValue )
+  }
+
+  public removeIngredient(index: number){
+    this.ingredients.removeAt(index);
+    this.cdr.markForCheck();
+  }
+
+  public addParamProperty(param: any){
+    const paramPropertyForm = this.fb.group({
+      '': ['', [Validators.required]]
+    });
+    this.getParamsProp(param).push(paramPropertyForm);
+    this.cdr.markForCheck();
+  }
+
+  public saveParamProp(paramKey: string, paramIndex: number, propType: string, propValue: string){
+    const paramPropertyForm = this.fb.group({
+      [propType] : [this.checkTypeParamsProp(propValue) ? +propValue : propValue, [Validators.required]]
+    });
+    this.getParamsProp(paramKey).push(paramPropertyForm);
+    this.getParamsProp(paramKey).removeAt(paramIndex);
+    this.cdr.markForCheck();
+  }
+
+  public getParamsProp(prop : any){
+    return (this.params.controls[`${prop}`] as FormArray);
+  }
+
+  public checkTypeParamsProp(prop: any): boolean {
+    return isFinite(prop);
   }
 
   public comeBack(){
