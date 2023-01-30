7b37591392c9b18b9faf20f5e03730bf2f60cdcc
ivanov6444 <a.ivanov6444@gmail.com>
2023-01-30 13:12:31 +0200

    after build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  urlPizzaAll: 'http://localhost:3001/pizza',
+  urlUsersAll: 'http://localhost:3001/users',
+  urlCommentsAll: 'http://localhost:3001/comments',
+  urlOrdersAll: 'http://localhost:3001/orders',
+  urlSliderData: 'http://localhost:3001/slider'
 };
